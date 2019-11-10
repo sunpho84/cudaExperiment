3f7cdfc23688f0d0ed61eecbdc86019f4d375fe5
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2019-11-10 22:38:36 +0100

    in main

--- main.cpp
+++ main.cpp
@@ -1,33 +1,33 @@
 //#include "global.hpp"
 
-__managed__ double* x;
-__managed__ double* y;
-
 const int N=1<<20;
 
-__host__ __device__
-void add(double* x,double* y,int i)
+__device__ 
+void _add(double* x,double* y,int i)
 {
       y[i] = x[i] + y[i];
 }
 
 __global__
-void add(int n)
+void add(double*x,double*y,int n)
 {
   int index = blockIdx.x * blockDim.x + threadIdx.x;
   int stride = blockDim.x * gridDim.x;
   for (int i = index; i < n; i += stride)
-    add(x,y,i);
+    _add(x,y,i);
 }
 
 int main()
 {
+  double* x;
+  double* y;
+  
   cudaMallocManaged(&x,N*sizeof(double));
   cudaMallocManaged(&y,N*sizeof(double));
   
   int blockSize = 256;
   int numBlocks = (N + blockSize - 1) / blockSize;
-  add<<<numBlocks, blockSize>>>(N);
+  add<<<numBlocks, blockSize>>>(x,y,N);
   cudaDeviceSynchronize();
   
   cudaFree(x);
