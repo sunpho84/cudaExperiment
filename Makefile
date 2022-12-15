d8e33d4d4a06fcb2c7ef8d92c483921d07a8941f
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2022-12-15 14:15:40 +0100

    Adding lambda test

--- Makefile
+++ Makefile
@@ -1,8 +1,15 @@
+test: test.cpp
+	nvcc -x cu -o test test.cpp -g -O0 --extended-lambda -std=c++17  --keep -G
+
+lambda: lambda.cpp Makefile
+	clang++ -xcuda lambda.cpp -o lambda --std=c++17 --cuda-gpu-arch=sm_70 -L/usr/local/cuda-11.2/targets/x86_64-linux/lib/ -lcudart_static -ldl -lrt -pthread -Wno-unknown-cuda-version
+
+
 main: main.o global.o global.hpp Makefile
 	nvcc -o main main.o global.o -g -O0
 
 main.o: main.cpp global.hpp Makefile
-	nvcc -x cu -c -o main.o main.cpp -g -O0
+	nvcc --extended-lambda  -x cu -c -o main.o main.cpp -g -O0
 
 global.o: global.cpp global.hpp Makefile
 	nvcc -x cu -c -o global.o global.cpp -g -O0
--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <cstdio>
 #include "global.hpp"
 
 const int N=1<<20;
@@ -19,6 +20,38 @@ void add(int n)
     add(x,y,i);
 }
 
+  template <typename IMin,
+	    typename IMax,
+	    typename F>
+  __global__
+  void cuda_generic_kernel(const IMin min,
+			   const IMax max,
+			   F f)
+  {
+    const auto i=min+blockIdx.x*blockDim.x+threadIdx.x;
+    if(i<max)
+      f(i);
+  }
+
+struct A
+{
+  A()
+  {
+    printf("creating A\n");
+  }
+  
+  A(const A&)
+  {
+    printf("copying A\n");
+  }
+  
+  __host__ __device__
+  ~A()
+  {
+    printf("destroying A\n");
+  }
+};
+
 int main()
 {
   cudaMallocManaged(&x,N*sizeof(double));
@@ -27,8 +60,20 @@ int main()
   int blockSize = 256;
   int numBlocks = (N + blockSize - 1) / blockSize;
   add<<<numBlocks, blockSize>>>(N);
-  
   cudaDeviceSynchronize();
+  
+  printf("---------\n");
+
+  A a;
+  
+  cuda_generic_kernel<<<10,10>>>(0,100,
+				 [a] __device__(const int i)
+  {
+  });
+  cudaDeviceSynchronize();
+  
+  printf("---------\n");
+  
   
   cudaFree(x);
   cudaFree(y);
